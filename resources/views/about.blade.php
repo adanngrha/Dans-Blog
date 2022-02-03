91192ad88c7c2a3993198ae92cbf13b7caefedef
Adan Nugraha <contact.ardana@gmail.com>
2022-02-03 11:05:08 +0700

    route and views

---
+++ resources/views/about.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Dan's Blog | About</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <h1>Halaman About</h1>
+    <h2>Adan Nugraha</h2>
+    <img src="img/azuki2.png" alt="gambar" width="200">
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/blog', function () {
+    return view('blog', [
+        "name" => "Adan Nugraha",
+        "email" => "adangrha@email.com",
+        "image" => "azuki2.png"
+    ]);
 });
