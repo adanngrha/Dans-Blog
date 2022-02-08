2311010ab76baf1f10a5b54593f3d31fe6a3dd45
Adan Nugraha <contact.ardana@gmail.com>
2022-02-08 09:26:55 +0700

    authenticatin, middleware

--- resources/views/auth/login/index.blade.php
+++ resources/views/auth/login/index.blade.php
@@ -12,17 +12,32 @@
             </div>
             @endif
 
+            @if (session()->has('loginError'))
+                <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                {{ session('loginError') }}
+                <button button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+            @endif
+
             <main class="form-signin">
-            <form>
+            <form action="/login" method="POST">
+                @csrf
                 {{-- <img class="mb-4" src="../assets/brand/bootstrap-logo.svg" alt="" width="72" height="57"> --}}
                 <h1 class="h3 mb-5 fw-normal text-center mt-5 ">Please Sign in</h1>
 
                 <div class="form-floating">
-                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-                <label for="floatingInput">Email address</label>
+                <input type="text" class="form-control @error('username')
+                    is-invalid
+                @enderror" id="username" placeholder="Your username" name="username" required value="{{ old('username') }}">
+                <label for="username">Username</label>
+                    @error('username')
+                        <div class="invalid-feedback">
+                        {{ $message }}
+                        </div>
+                    @enderror
                 </div>
                 <div class="form-floating">
-                <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                <input type="password" class="form-control" id="floatingPassword" placeholder="Password" name="password" required>
                 <label for="floatingPassword">Password</label>
                 </div>
 
